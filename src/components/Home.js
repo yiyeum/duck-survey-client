dfdaf4baecd49159d5134241688141c875cc6475
Kate Yeeum <yeeumm@gmail.com>
2019-03-09 23:45:43 -0800

    Home - Content updated
    - content / button added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 const Home = () => {
-    return (
-        <div>
-          Home
-        </div>
-    );
-}
+  return (
+    <div className="home-wrapper">
+            <h1>Welcome!</h1>
+            <p className="main-text">By filling out this 3 minutes form,</p>
+            <p className="main-text">you will help the scientists understand</p>
+            <p className="main-text">how ducks are being fed in parks around the world.</p>
 
+            <button type="button" className="home-ready-btn bounce animated infinite">I'm Ready</button>
+        </div>
+      );
+  }
+  
 export default Home;
