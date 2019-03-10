11c847e2fd341844d0c233f34e736af84f8cc616
Kate Yeeum <yeeumm@gmail.com>
2019-03-10 00:20:22 -0800

    Place / Duck - Content updated
    - stepper, textfield added

--- src/components/Duck.js
+++ src/components/Duck.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Stepper from './Stepper';
 
 const Duck = () => {
-    return (
-        <div>
-          Duck
-        </div>
-    );
+  return (
+    <div className="survey-wrapper">
+      <Stepper step={3} />
+      <p className="question">3. How many ducks did you feed?</p>
+      <TextField
+        label="Number of ducks"
+        name="numberOfDucks"
+        type="number"
+        InputLabelProps={{
+          shrink: true,
+        }}
+        margin="normal"
+      />
+      <br />
+      <p className="error-message">Error</p>
+      <button type="button" className="survey-prev-button">Prev</button>
+      <button type="button" className="survey-next-button">Next</button>
+    </div>
+  );
 }
 
 export default Duck;
--- src/components/Place.js
+++ src/components/Place.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import Stepper from './Stepper';
+import TextField from '@material-ui/core/TextField';
 
 const Place = () => {
-    return (
-        <div>
-          Place
-        </div>
-    );
+  return (
+    <div className="survey-wrapper">
+      <Stepper step={2} />
+      <p className="question">2. Where did you feed ducks?</p>
+      <TextField
+        label="Place Name"
+        name="place"
+        margin="normal"
+      />
+      <br />
+      <p className="error-message">Error</p>
+      <button type="button" className="survey-prev-button">Prev</button>
+      <button type="button" className="survey-next-button">Next</button>
+    </div>
+  );
 }
 
 export default Place;
