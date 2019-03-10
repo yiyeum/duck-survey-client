c7aad90fbe9c73ad5a09cb97adf469c84d04ca27
Kate Yeeum <yeeumm@gmail.com>
2019-03-10 00:42:40 -0800

    Review / Complete - Content updated

--- src/components/Complete.js
+++ src/components/Complete.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const Complete = () => {
-    return (
-        <div>
-          Complete
-        </div>
-    );
+  return (
+    <div className="home-wrapper">
+      <h1>Thank you!</h1>
+      <p className="main-text">Thanks for your time to complete this survey.</p>
+      <button type="button" className="result-btn">See the result</button>
+    </div>
+  );
 }
 
 export default Complete;
--- src/components/Review.js
+++ src/components/Review.js
@@ -1,11 +1,43 @@
 import React from 'react';
 
 const Review = () => {
-    return (
-        <div>
-          Review
-        </div>
-    );
+  return (
+    <div className="survey-wrapper">
+      <p className="subtitle">Review</p>
+      <div className="review-wrapper">
+        <table className="review-table">
+          <tbody>
+            <tr>
+              <th>Fed time</th>
+              <td>time</td>
+            </tr>
+            <tr>
+              <th>Place</th>
+              <td>Place</td>
+            </tr>
+            <tr>
+              <th>Number of ducks</th>
+              <td>Number of ducks</td>
+            </tr>
+            <tr>
+              <th>Food type</th>
+              <td>Food type</td>
+            </tr>
+            <tr>
+              <th>Food</th>
+              <td>Food</td>
+            </tr>
+            <tr>
+              <th>Food amount</th>
+              <td>Food amount</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <button type="button" className="survey-prev-button">Prev</button>
+      <button type="button" className="survey-next-button">Submit</button>
+    </div>
+  );
 }
 
 export default Review;
